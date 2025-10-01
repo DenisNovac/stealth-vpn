154f0b853228ccce1297f48f1c4667dedbd909ea
DenisNovac <ponydnk@gmail.com>
2025-10-01 20:36:28 +0400

    all files

--- keys/generate.sh
+++ keys/generate.sh
@@ -6,3 +6,6 @@ wg genkey > client
 wg pubkey < client > client.pub
 
 wg genkey > preshared
+
+# secret for wstunnel
+read -r -n 64 path_prefix < <(LC_ALL=C tr -dc "[:alnum:]" < /dev/urandom); echo $path_prefix > tunnel
